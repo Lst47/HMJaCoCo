ed6b80185972554fcd5d1411e96d58006a6c5a67
lst47 <linh20121@gmail.com>
2022-07-30 21:09:11 +0300

    100% coverege

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,5 +1,5 @@
+package ru.netology.statistic;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
@@ -9,6 +9,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void indMaxEnd(){
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 120};
+        long expected = 120;
 
         long actual = service.findMax(incomesInBillions);
 
